8aa77e0fc9f3f35379e849c35a18b6f25013de4c
bmcgauley <brian@mcgauley.com>
2025-03-24 12:18:03 -0700

    Enhance Next.js image configuration in next.config.js. Added settings for image optimization, SVG handling, content security policy, and remote patterns to improve image management and security.

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
       allowedOrigins: ['localhost:3001']
     }
   },
+  images: {
+    domains: [],
+    unoptimized: true,
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [],
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
