f5808f5403dc960911c48f67561702cd7830dd15
bmcgauley <brian@mcgauley.com>
2025-03-24 12:33:46 -0700

    Update image formats in Next.js configuration to include only PNG and JPEG formats, enhancing clarity and compatibility for image handling.

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
     dangerouslyAllowSVG: true,
-    formats: ['image/avif', 'image/webp'],
+    formats: ['image/png', 'image/jpeg', 'image/jpg'],
     remotePatterns: [
       {
         protocol: 'https',
